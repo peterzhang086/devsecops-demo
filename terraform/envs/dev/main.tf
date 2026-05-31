09d6bc70eeb628be8b50853a8c5e08d691d3e8eb
PeterZhang <peterzhang086@gmail.com>
2026-05-31 23:53:50 +0800

    fix: upgrade CI EKS policy to ClusterAdmin to allow namespace creation

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -68,7 +68,7 @@ resource "aws_eks_access_entry" "github_actions" {
 resource "aws_eks_access_policy_association" "github_actions_deploy" {
   cluster_name  = module.eks.cluster_name
   principal_arn = module.github_oidc.role_arn
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSAdminPolicy"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
     type = "cluster"
